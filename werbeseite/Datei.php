55f64ef75d764037caafd4ca2dd72dc0418e02d6
welly <andreaswelly.2002@gmail.com>
2022-11-04 17:47:07 +0100

    Aufgabe 8

---
+++ werbeseite/Datei.php
@@ -0,0 +1,44 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"]== "POST"){
+
+        $name =trim($_POST['benutzer']??NULL);
+        $email = strtolower($_POST['email'] ?? NULL);
+        $language = $_POST['language']?? NULL;
+        $datenschutz = $_POST['datenschutz'];
+
+
+        function mail_pruefen($email){
+            $notallowed=['rcpt.at', 'damnthespam.at', 'wegwerfmail.at', 'trashmail.at' ];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                return false;
+            }
+            else{
+                if(in_array($email, $notallowed)){
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            }
+        }
+        $fehler = NULL;
+        if(!preg_match("/^([a-zA-Z' ]+)$/", $name)){
+            $fehler = "Name ist nicht gultig!";
+        }
+        elseif(mail_pruefen($email)==false){
+            $fehler = "E-Mail ist nicht gultig";
+        }
+
+        if(isset($fehler)){
+            header("Location: http://localhost:63342/werbeseite/E-Mensa/werbeseite/werbeseite.php?status=". $fehler);
+        }
+        else{
+            $file= fopen("newsletter.txt", "a") or die("Fehler aufgetreten");
+            $data = "Name: ".$name. "\nE-Mail: ".$email. "\nSprache: ".$language."\n";
+            fwrite($file, $data);
+            fclose($file);
+            header("Location: http://localhost:63342/werbeseite/E-Mensa/werbeseite/werbeseite.php?status=erfolgreich%22");
+        }
+    }
+
+?>
