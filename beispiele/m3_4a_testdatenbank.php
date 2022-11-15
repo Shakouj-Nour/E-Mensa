23a3203d902bdb93948339cc644ee8260cea9a6a
berrybeaver <andreaswelly.2002@gmail.com>
2022-11-15 15:57:16 +0100

    aufgabe4

---
+++ beispiele/m3_4a_testdatenbank.php
@@ -0,0 +1,7 @@
+<?php
+$link = mysqli_connect(
+    "127.0.0.1", // Host der Datenbank
+    "root", // Benutzername zur Anmeldung
+    "root", // Passwort zur Anmeldung
+    "emensawerbeseite", // Auswahl der Datenbank
+    3306);
