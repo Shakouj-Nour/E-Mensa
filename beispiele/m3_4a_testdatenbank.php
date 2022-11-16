1067c44f7730480d297f73223dc5c99e67a94ecb
berrybeaver <andreaswelly.2002@gmail.com>
2022-11-16 14:06:56 +0100

    aufgabe4

--- beispiele/m3_4a_testdatenbank.php
+++ beispiele/m3_4a_testdatenbank.php
@@ -1,2 +1,18 @@
 <?php
-mysqli_connect();
+$link = mysqli_connect(
+    "127.0.0.1", // Host der Datenbank
+    "root", // Benutzername zur Anmeldung
+    "root", // Passwort zur Anmeldung
+    "emensawerbeseite", // Auswahl der Datenbank
+    3306);
+
+if (!$link) {
+    echo "Verbindung fehlgeschlagen: ", mysqli_connect_error();
+    exit();
+}
+$sql = "SELECT name FROM gericht";
+$result = mysqli_query($link, $sql);
+while ($row = mysqli_fetch_row($result)) {
+    var_dump($row);
+}
+
