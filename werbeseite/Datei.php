de42be3686d9822fb80657015196b32faa1362c6
berrybeaver <andreaswelly.2002@gmail.com>
2022-11-18 11:37:46 +0100

    aufgabe10

--- werbeseite/Datei.php
+++ werbeseite/Datei.php
@@ -5,8 +5,9 @@
 - Andreas Welly Octavianus, 3541951
  */
     if($_SERVER["REQUEST_METHOD"]== "POST"){
-
+        //Unnötige Leerzeichen aus dem eingegebenen Namen entfernen
         $name =trim($_POST['benutzer']??NULL);
+        //Alle Buchstaben in Kleinbuchstaben setzen
         $email = strtolower($_POST['email'] ?? NULL);
         $language = $_POST['language']?? NULL;
         $datenschutz = $_POST['datenschutz'];
@@ -14,22 +15,27 @@
 
         function mail_pruefen($email){
             $notallowed=['rcpt.at', 'damnthespam.at', 'wegwerfmail.at', 'trashmail.at' ];
+            //prüft, ob die Eingabe in email-form ist
             if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
                 return false;
             }
             else{
+                //wenn $notallowed in email gefunden wird, wird false zurückgegeben
                 if(in_array($email, $notallowed)){
                     return false;
                 }
+                //sonst True
                 else{
                     return true;
                 }
             }
         }
         $fehler = NULL;
+        //wenn eingegebene Name ein Element enthält, das nicht in Pattern enthalten ist, es wird ein Fehler gesetzt
         if(!preg_match("/^([a-zA-Z' ]+)$/", $name)){
             $fehler = "Name ist nicht gultig!";
         }
+        //wenn email nicht gultig ist
         elseif(mail_pruefen($email)==false){
             $fehler = "E-Mail ist nicht gultig";
         }
