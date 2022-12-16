4b7b0c3b26ee6d860af7af23bcc1d773962d17c6
berrybeaver <andreaswelly.2002@gmail.com>
2022-12-16 10:21:06 +0100

    A4

--- beispiele/m5_4.sql
+++ beispiele/m5_4.sql
@@ -5,6 +5,8 @@ CREATE VIEW view_anmeldungen AS
     SELECT anzahlanmeldungen, name FROM benutzer ORDER BY
     anzahlanmeldungen DESC ; -- 2
 
-CREATE VIEW view_kategoriegerichte_vegetarisch AS
-    SELECT name FROM gericht WHERE vegetarisch = 1, kategorie.name AS kategorie  FROM
-    gericht LEFT JOIN kategorie on gericht.id = kategorie.id;
+create view view_kategoriegerichte_vegetarisch as
+    select gericht.name as gericht, kategorie.name as kategorie from gericht
+    join gericht_hat_kategorie on gericht.id = gericht_hat_kategorie.gericht_id
+    right join kategorie on kategorie.id = gericht_hat_kategorie.kategorie_id and
+    gericht.vegetarisch = 1; -- 3
